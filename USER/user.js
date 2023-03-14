687e92a454991783116a0c841498c5b3e84a23b0
lairacouto <contato.lairacouto@gmail.com>
2023-03-14 15:00:07 -0300

    Testes array

---
+++ USER/user.js
@@ -0,0 +1,22 @@
+
+
+const lista = [
+    {
+        id: 1,
+        nome: "Teste"
+    },
+    {
+        id: 2,
+        nome: "Teste"
+    },
+    {
+        id: 3,
+        nome: "Teste"
+    },
+    {
+        id: 4,
+        nome: "Teste"
+    },
+]
+
+document.getElementById('lista').innerHtml='hdhsdjsdj';
